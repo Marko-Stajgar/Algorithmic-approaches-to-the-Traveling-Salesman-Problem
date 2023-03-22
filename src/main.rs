d8ada377b10b196c56b5314acaf10ccef6eb0adf
Marko Stajgar <Marko Stajgar>
2023-03-22 17:09:57 -0400

    Update: Implementation of console command history

--- src/main.rs
+++ src/main.rs
@@ -25,18 +25,28 @@ fn main()
         }))
         .add_startup_system(setup)
         .add_system(session_time)
-        .add_system(text_input)
+        .add_system(console_input)
         .add_system(add_vertex_to_graph)
         .run();
 }
 
 #[derive(Component)]
-struct ConsoleTextInput;
+struct ConsoleInputText;
+
+#[derive(Component)]
+struct ConsolePastCommand1;
+
+#[derive(Component)]
+struct ConsolePastCommand2;
+
+#[derive(Component)]
+struct ConsolePastCommand3;
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>)
 {
     commands.spawn(Camera2dBundle::default());
 
+    // Spawns a text bundle representing initial console mark ":$ "
     commands.spawn((
         // Create a TextBundle that has a Text with a single section.
         TextBundle::from_section(
@@ -61,6 +71,7 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>)
             }),
     ));
 
+    // Spawns a text budnle representing the user console input in realtime
     commands.spawn((
         // Create a TextBundle that has a Text with a single section.
         TextBundle::from_section(
@@ -83,7 +94,85 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>)
                 },
                 ..default()
             }),
-        ConsoleTextInput,
+        ConsoleInputText,
+    ));
+
+    // Spawns a text bundle representing past command 1 from user console input
+    commands.spawn((
+        // Create a TextBundle that has a Text with a single section.
+        TextBundle::from_section(
+            // Accepts a `String` or any type that converts into a `String`, such as `&str`
+            "",
+            TextStyle {
+                font: asset_server.load("fonts/FiraSans-ExtraLight.ttf"),
+                font_size: 30.0,
+                color: Color::WHITE,
+            },
+        ) // Set the alignment of the Text
+            .with_text_alignment(TextAlignment::Left)
+            // Set the style of the TextBundle itself.
+            .with_style(Style {
+                position_type: PositionType::Absolute,
+                position: UiRect {
+                    bottom: Val::Px(40.0),
+                    left: Val::Px(15.0),
+                    ..default()
+                },
+                ..default()
+            }),
+        ConsolePastCommand1,
+    ));
+
+    // Spawns a text bundle representing past command 2 from user console input
+    commands.spawn((
+        // Create a TextBundle that has a Text with a single section.
+        TextBundle::from_section(
+            // Accepts a `String` or any type that converts into a `String`, such as `&str`
+            "",
+            TextStyle {
+                font: asset_server.load("fonts/FiraSans-ExtraLight.ttf"),
+                font_size: 30.0,
+                color: Color::WHITE,
+            },
+        ) // Set the alignment of the Text
+            .with_text_alignment(TextAlignment::Left)
+            // Set the style of the TextBundle itself.
+            .with_style(Style {
+                position_type: PositionType::Absolute,
+                position: UiRect {
+                    bottom: Val::Px(75.0),
+                    left: Val::Px(15.0),
+                    ..default()
+                },
+                ..default()
+            }),
+        ConsolePastCommand2,
+    ));
+
+    // Spawns a text bundle representing past command 3 from user console input
+    commands.spawn((
+        // Create a TextBundle that has a Text with a single section.
+        TextBundle::from_section(
+            // Accepts a `String` or any type that converts into a `String`, such as `&str`
+            "",
+            TextStyle {
+                font: asset_server.load("fonts/FiraSans-ExtraLight.ttf"),
+                font_size: 30.0,
+                color: Color::WHITE,
+            },
+        ) // Set the alignment of the Text
+            .with_text_alignment(TextAlignment::Left)
+            // Set the style of the TextBundle itself.
+            .with_style(Style {
+                position_type: PositionType::Absolute,
+                position: UiRect {
+                    bottom: Val::Px(110.0),
+                    left: Val::Px(15.0),
+                    ..default()
+                },
+                ..default()
+            }),
+        ConsolePastCommand3,
     ));
 }
 
@@ -96,19 +185,25 @@ fn session_time(mut windows: Query<&mut Window>, time: Res<Time>)
 }
 
 
-// This system takes keyboard input and updates the console text on screen acordingly
-fn text_input(
+// This system takes keyboard input and updates the console text on screen accordingly
+fn console_input(
     mut commands: Commands,
     mut asset_server: Res<AssetServer>,
     mut char_evr: EventReader<ReceivedCharacter>,
     keys: Res<Input<KeyCode>>,
+    mut console_past_command3: Local<String>,
+    mut console_past_command2: Local<String>,
+    mut console_past_command1: Local<String>,
     mut string: Local<String>,
-    mut query: Query<&mut Text, With<ConsoleTextInput>>)
+    mut console_input_param_set: ParamSet<(
+        Query<&mut Text, With<ConsoleInputText>>,
+        Query<&mut Text, With<ConsolePastCommand1>>,
+        Query<&mut Text, With<ConsolePastCommand2>>,
+        Query<&mut Text, With<ConsolePastCommand3>>,
+    )>)
 {
     for ev in char_evr.iter()
     {
-        println!("Got char: '{}'", ev.char);
-
         if ev.char == '\x08'
         {
             string.pop();
@@ -119,20 +214,42 @@ fn text_input(
             string.push(ev.char);
         }
 
-        for mut text in &mut query
+        for mut text in console_input_param_set.p0().iter_mut()
         {
-            text.sections[0].value = format!("{}", *string)
+            text.sections[0].value = format!("{}", *string);
         }
     }
 
     if keys.just_pressed(KeyCode::Return)
     {
+        *console_past_command3 = console_past_command2.to_string();
+        *console_past_command2 = console_past_command1.to_string();
+        *console_past_command1 = string.to_string();
+
         println!("Text input: {}", *string);
+        println!("Console past command1: {}", *console_past_command1);
+        println!("Console past command2: {}", *console_past_command2);
+        println!("Console past command3: {}", *console_past_command3);
         string.clear();
 
-        for mut text in &mut query
+        for mut console_past_command1_text in &mut console_input_param_set.p1().iter_mut()
         {
-            text.sections[0].value = format!("{}", *string)
+            console_past_command1_text.sections[0].value = format!("{}", *console_past_command1);
+        }
+
+        for mut console_past_command2_text in &mut console_input_param_set.p2().iter_mut()
+        {
+            console_past_command2_text.sections[0].value = format!("{}", *console_past_command2);
+        }
+
+        for mut console_past_command3_text in &mut console_input_param_set.p3().iter_mut()
+        {
+            console_past_command3_text.sections[0].value = format!("{}", *console_past_command3)
+        }
+
+        for mut console_input_text in &mut console_input_param_set.p0().iter_mut()
+        {
+            console_input_text.sections[0].value = format!("{}", *string);
         }
     }
 }
@@ -158,7 +275,8 @@ fn add_vertex_to_graph(
 // This function draws a circle on the app canvas
 fn draw_circle(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<ColorMaterial>>)
 {
-    commands.spawn(MaterialMesh2dBundle {
+    commands.spawn(MaterialMesh2dBundle
+    {
         mesh: meshes.add(shape::Circle::new(10.).into()).into(),
         material: materials.add(ColorMaterial::from(Color::WHITE)),
         transform: Transform::from_translation(Vec3::new(0., 0., 0.)),
